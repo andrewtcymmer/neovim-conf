4a7ee093d028617b6898eb026993f1fec5c4d550
andrewtcymmer <29125201+andrewtcymmer@users.noreply.github.com>
2021-09-16 14:28:54 -0400

    Replace nvim-compe with nvim-cmp
    Found a note in nvim-compe that it is deprecated in favor of nvim-cmp.
    New version includes full Lua configuration for keybinds.
    Replaced compe configuration with cmp in init.lua, using defaults from github readme page.
    Added ctrl+j ctrl+k keybinds to navigate list up/down.

--- init.lua
+++ init.lua
@@ -42,88 +42,31 @@ vim.cmd [[silent! colorscheme everforest]]
 require'lspconfig'.tsserver.setup{}
 
 -- autocompletion
-require'compe'.setup {
-  enabled = true;
-  autocomplete = true;
-  debug = false;
-  min_length = 1;
-  preselect = 'enable';
-  throttle_time = 80;
-  source_timeout = 200;
-  resolve_timeout = 800;
-  incomplete_delay = 400;
-  max_abbr_width = 100;
-  max_kind_width = 100;
-  max_menu_width = 100;
-  documentation = {
-    border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
-    winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
-    max_width = 120,
-    min_width = 60,
-    max_height = math.floor(vim.o.lines * 0.3),
-    min_height = 1,
-  };
-
-  source = {
-    path = true;
-    buffer = true;
-    calc = true;
-    nvim_lsp = true;
-    nvim_lua = true;
-    vsnip = true;
-    ultisnips = true;
-    luasnip = true;
-  };
-}
-
--- ### compe keybindings ###
-
-local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-    local col = vim.fn.col('.') - 1
-    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
-end
-
--- Use (s-)tab to:
---- move to prev/next item in completion menuone
---- jump to prev/next snippet's placeholder
-_G.tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-n>"
-  elseif check_back_space() then
-    return t "<Tab>"
-  else
-    return vim.fn['compe#complete']()
-  end
-end
-_G.s_tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-p>"
-  else
-    -- If <S-Tab> is not working in your terminal, change it to <C-h>
-    return t "<S-Tab>"
-  end
-end
-
-vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-
--- vim.api.nvim_set_keymap('i', '<CR>', "v:lua.carriage_accept()", {expr = true})
-
--- ### end compe keybindings ###
-
-
--- For some future version, these might work. In 0.5 gotta use vimscript for keybinding.
--- vim.api.nvim_buf_set_keymap(0, 'i', '<C-Space>', "<cmd>lua require('compe').complete()<CR>", { expr = true, noremap = true, silent = true })
--- vim.api.nvim_buf_set_keymap(0, 'i', '<CR>', "compe#confirm('<CR>')", { expr = true, noremap = true, silent = true })
--- vim.api.nvim_buf_set_keymap(0, 'i', '<C-e>', "compe#close('<C-e>')", { expr = true, noremap = true, silent = true })
--- vim.api.nvim_buf_set_keymap(0, 'i', '<C-f>', "compe#scroll({ 'delta': +4 })", { expr = true, noremap = true, silent = true })
--- vim.api.nvim_buf_set_keymap(0, 'i', '<C-d>', "compe#scroll({ 'delta': -4 })", { expr = true, noremap = true, silent = true })
+local cmp = require'cmp'
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ['<C-k>'] = cmp.mapping.select_prev_item(),
+    ['<C-j>'] = cmp.mapping.select_next_item(),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    })
+  },
+  sources = {
+    { name = '...' },
+    { name = 'buffer' }
+  }
+})
 
 -- research these and enable later
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,7 +2,16 @@ return require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/nvim-compe'  -- autocompletion with typescript import support
+  -- Install nvim-cmp, and buffer source as a dependency
+  use 'hrsh7th/vim-vsnip'
+  use 'hrsh7th/cmp-buffer'
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'hrsh7th/vim-vsnip',
+      'hrsh7th/cmp-buffer',
+    }
+  }
   -- colors
   use 'sainnhe/everforest' -- compatible with treesitter
   use 'sainnhe/gruvbox-material' -- compatible with treesitter
