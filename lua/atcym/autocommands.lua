21561c10763a226cfaf44a8e5da24259fa8b79d6
andrewtcymmer <29125201+andrewtcymmer@users.noreply.github.com>
2022-04-12 08:19:16 -0500

    Removed vimscript ftplugin files.

--- lua/atcym/autocommands.lua
+++ lua/atcym/autocommands.lua
@@ -8,3 +8,19 @@ vim.cmd [[
 
 -- TODO: investigate this
 -- autocmd BufWritePre *.tf lua vim.lsp.buf.formatting_sync()
+
+-- TODO: factor this into a rust-only config file?
+vim.cmd [[
+  augroup Racer
+    autocmd!
+    autocmd FileType rust nmap <buffer> gd          <Plug>(rust-def)
+    autocmd FileType rust nmap <buffer> gs          <Plug>(rust-def-split)
+    autocmd FileType rust nmap <buffer> gx          <Plug>(rust-def-vertical)
+    autocmd FileType rust nmap <buffer> gt          <Plug>(rust-def-tab)
+    autocmd FileType rust nmap <buffer> <leader> gd <Plug>(rust-doc)
+    autocmd FileType rust nmap <buffer> <leader> gD <Plug>(rust-doc-tab)
+  augroup END
+
+  " Function keys
+  nmap <F9> :RustFmt<CR>
+]]
