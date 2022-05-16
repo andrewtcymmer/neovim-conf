ce7700774db94cb4ae28abd5e89fe51c82e34303
andrewtcymmer <29125201+andrewtcymmer@users.noreply.github.com>
2022-05-15 22:51:11 -0500

    Packer self-install code added; use() syntax short
    This code checks if packer is installed in the "packpath" of neovim. If
    it is not, it clones the git repository to the right location.
    Instructions for installing from a fresh install added to the readme.
    "Heavy" syntax of putting tables { } for each use() argument removed.

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # neovim-conf
 My NeoVim configuration.
 
-# Quick Start
-Install Packer with `git clone https://github.com/wbthomason/packer.nvim`. Run `:PackerCompile` then `:PackerInstall` from inside nvim.  
+# From a Fresh Start
+From an install with no plugins (including packer) do this:
+1. `git clone git@github.com:andrewtcymmer/neovim-conf ~/.config/nvim/`
+1. `nvim` to open. You will see an error, but packer has been installed. Exit (`ZQ`)
+1. `nvim` to open again. You will see the same error. Run `:PackerInstall`.
+1. `nvim` to open yet again. You will see an error about tree-sitter. However, tree-sitter updates itself after this error, which will make it go away on next open.
+1. `nvim` is now ready to rip!
 
--- lua/atcym/packer.lua
+++ lua/atcym/packer.lua
@@ -1,28 +1,51 @@
+local fn = vim.fn
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  } 
+  vim.notify 'Installing Packer. Close and re-open NeoVim.'
+  vim.cmd [[packadd packer.nvim]]
+end
+
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
 return require('packer').startup(
   function(use)
     -- Packer can manage itself
-    use { 'wbthomason/packer.nvim' }
-    use { 'neovim/nvim-lspconfig' }
+    use 'wbthomason/packer.nvim'
+    use 'neovim/nvim-lspconfig'
     use {
       'nvim-treesitter/nvim-treesitter',
       run = ':TSUpdate',
       config = [[require('atcym.plugins.nvim-treesitter')]],
-    }
+    } 
     -- Install nvim-cmp, and buffer source as a dependency
-    use { 'hrsh7th/vim-vsnip' }
-    use { 'hrsh7th/cmp-buffer' }
-    use { 'hrsh7th/cmp-nvim-lsp' }
+    use 'hrsh7th/vim-vsnip'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-nvim-lsp'
     use {
       'hrsh7th/nvim-cmp',
       requires = {
         'hrsh7th/vim-vsnip',
         'hrsh7th/cmp-buffer',
         'hrsh7th/cmp-nvim-lsp',
-      }
-    }
+      } 
+    } 
     -- colors
-    use { 'andersevenrud/nordic.nvim' }
+    use 'andersevenrud/nordic.nvim'
+
+    -- update packer
+    if PACKER_BOOTSTRAP then
+      requre'packer'.sync()
+    end
   end
 )
-
-
